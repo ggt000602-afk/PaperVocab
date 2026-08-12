b63245b6022875e4d57e02f30f9db166452207de
ggt000602-afk <ggt000602@gmail.com>
2026-08-12 14:28:17 +0900

    Add cryogenic boiling and transport vocabulary part 5

---
+++ data_more5.js
@@ -0,0 +1,93 @@
+addBulkWords(`saturated liquid|/ˈsætʃəreɪtɪd ˈlɪkwɪd/|새처레이티드 리퀴드|포화 액체|Cryogenics
+saturated vapor|/ˈsætʃəreɪtɪd ˈveɪpər/|새처레이티드 베이퍼|포화 증기|Cryogenics
+subcooled liquid|/ˌsʌbˈkuːld ˈlɪkwɪd/|서브쿨드 리퀴드|과냉 액체|Cryogenics
+saturation state|/ˌsætʃəˈreɪʃən steɪt/|새처레이션 스테이트|포화 상태|Cryogenics
+critical pressure|/ˈkrɪtɪkəl ˈprɛʃər/|크리티컬 프레셔|임계압력|Cryogenics
+critical temperature|/ˈkrɪtɪkəl ˈtɛmprətʃər/|크리티컬 템퍼러처|임계온도|Cryogenics
+triple point|/ˈtrɪpəl pɔɪnt/|트리플 포인트|삼중점|Cryogenics
+normal boiling point|/ˈnɔːrməl ˈbɔɪlɪŋ pɔɪnt/|노멀 보일링 포인트|정상비점|Cryogenics
+thermophysical property|/ˌθɜːrmoʊˈfɪzɪkəl ˈprɑːpərti/|써모피지컬 프로퍼티|열물성|Cryogenics
+density ratio|/ˈdɛnsəti ˈreɪʃioʊ/|덴서티 레이쇼|밀도비|Cryogenics
+specific volume|/spəˈsɪfɪk ˈvɑːljuːm/|스페시픽 볼륨|비체적|Cryogenics
+enthalpy|/ˈɛnθəlpi/|엔탈피|엔탈피|Cryogenics
+specific enthalpy|/spəˈsɪfɪk ˈɛnθəlpi/|스페시픽 엔탈피|비엔탈피|Cryogenics
+internal energy|/ɪnˈtɜːrnəl ˈɛnərdʒi/|인터널 에너지|내부에너지|Cryogenics
+latent heat|/ˈleɪtənt hiːt/|레이턴트 히트|잠열|Cryogenics
+heat of vaporization|/hiːt əv ˌveɪpərəˈzeɪʃən/|히트 오브 베이퍼리제이션|기화잠열|Cryogenics
+thermal expansion|/ˈθɜːrməl ɪkˈspænʃən/|써멀 익스팬션|열팽창|Heat Transfer
+coefficient of thermal expansion|/ˌkoʊɪˈfɪʃənt əv ˈθɜːrməl ɪkˈspænʃən/|코이피션트 오브 써멀 익스팬션|열팽창계수|Heat Transfer
+thermal penetration depth|/ˈθɜːrməl ˌpɛnəˈtreɪʃən dɛpθ/|써멀 페네트레이션 뎁스|열침투 깊이|Heat Transfer
+thermal boundary layer|/ˈθɜːrməl ˈbaʊndəri ˈleɪər/|써멀 바운더리 레이어|열경계층|Heat Transfer
+heat capacity|/hiːt kəˈpæsəti/|히트 커패시티|열용량|Heat Transfer
+volumetric heat capacity|/ˌvɑːljəˈmɛtrɪk hiːt kəˈpæsəti/|볼류메트릭 히트 커패시티|체적 열용량|Heat Transfer
+thermal effusivity|/ˈθɜːrməl ˌɛfjuːˈsɪvəti/|써멀 에퓨시비티|열침투율|Heat Transfer
+heat conduction|/hiːt kənˈdʌkʃən/|히트 컨덕션|열전도|Heat Transfer
+one-dimensional conduction|/wʌn dɪˈmɛnʃənəl kənˈdʌkʃən/|원 디멘셔널 컨덕션|1차원 열전도|Heat Transfer
+inverse heat conduction|/ˈɪnvɜːrs hiːt kənˈdʌkʃən/|인버스 히트 컨덕션|역열전도|Heat Transfer
+axial conduction|/ˈæksiəl kənˈdʌkʃən/|액시얼 컨덕션|축방향 전도|Heat Transfer
+radial conduction|/ˈreɪdiəl kənˈdʌkʃən/|레이디얼 컨덕션|반경방향 전도|Heat Transfer
+heat spreading|/hiːt ˈsprɛdɪŋ/|히트 스프레딩|열확산, 열퍼짐|Heat Transfer
+thermal contact|/ˈθɜːrməl ˈkɑːntækt/|써멀 콘택트|열접촉|Heat Transfer
+contact resistance|/ˈkɑːntækt rɪˈzɪstəns/|콘택트 리지스턴스|접촉저항|Heat Transfer
+convective heat transfer|/kənˈvɛktɪv hiːt ˈtrænsfər/|컨벡티브 히트 트랜스퍼|대류 열전달|Heat Transfer
+radiative heat transfer|/ˈreɪdiətɪv hiːt ˈtrænsfər/|레이디에이티브 히트 트랜스퍼|복사 열전달|Heat Transfer
+heat transfer enhancement|/hiːt ˈtrænsfər ɪnˈhæ nsmənt/|히트 트랜스퍼 인핸스먼트|열전달 향상|Heat Transfer
+heat transfer deterioration|/hiːt ˈtrænsfər dɪˌtɪriəˈreɪʃən/|히트 트랜스퍼 디티리어레이션|열전달 저하|Heat Transfer
+thermal performance|/ˈθɜːrməl pərˈfɔːrməns/|써멀 퍼포먼스|열성능|Heat Transfer
+cooling performance|/ˈkuːlɪŋ pərˈfɔːrməns/|쿨링 퍼포먼스|냉각 성능|Heat Transfer
+heat removal|/hiːt rɪˈmuːvəl/|히트 리무벌|열제거|Heat Transfer
+wall temperature|/wɔːl ˈtɛmprətʃər/|월 템퍼러처|벽면 온도|Heat Transfer
+surface temperature|/ˈsɜːrfɪs ˈtɛmprətʃər/|서피스 템퍼러처|표면 온도|Heat Transfer
+bulk temperature|/bʌlk ˈtɛmprətʃər/|벌크 템퍼러처|벌크 온도|Heat Transfer
+liquid temperature|/ˈlɪkwɪd ˈtɛmprətʃər/|리퀴드 템퍼러처|액체 온도|Heat Transfer
+temperature difference|/ˈtɛmprətʃər ˈdɪfərəns/|템퍼러처 디퍼런스|온도차|Heat Transfer
+temperature excursion|/ˈtɛmprətʃər ɪkˈskɜːrʒən/|템퍼러처 익스커전|온도 급상승|Boiling
+temperature overshoot|/ˈtɛmprətʃər ˈoʊvərʃuːt/|템퍼러처 오버슈트|온도 오버슈트|Boiling
+preboiling regime|/priːˈbɔɪlɪŋ reɪˈʒiːm/|프리보일링 레짐|비등 전 영역|Boiling
+incipient boiling|/ɪnˈsɪpiənt ˈbɔɪlɪŋ/|인시피언트 보일링|초기 비등|Boiling
+fully developed nucleate boiling|/ˈfʊli dɪˈvɛləpt ˈnuːkliət ˈbɔɪlɪŋ/|풀리 디벨롭트 뉴클리에이트 보일링|완전 발달 핵비등|Boiling
+boiling regime|/ˈbɔɪlɪŋ reɪˈʒiːm/|보일링 레짐|비등 영역|Boiling
+boiling transition|/ˈbɔɪlɪŋ trænˈzɪʃən/|보일링 트랜지션|비등 전이|Boiling
+premature transition|/ˌpriːməˈtʃʊr trænˈzɪʃən/|프리머처 트랜지션|조기 전이|Boiling
+post-CHF|/poʊst siː eɪtʃ ɛf/|포스트 씨에이치에프|CHF 이후|Boiling
+critical condition|/ˈkrɪtɪkəl kənˈdɪʃən/|크리티컬 컨디션|임계 조건|Boiling
+CHF enhancement|/siː eɪtʃ ɛf ɪnˈhænsmənt/|씨에이치에프 인핸스먼트|CHF 향상|Boiling
+CHF prediction|/siː eɪtʃ ɛf prɪˈdɪkʃən/|씨에이치에프 프리딕션|CHF 예측|Boiling
+CHF correlation|/siː eɪtʃ ɛf ˌkɔːrəˈleɪʃən/|씨에이치에프 코럴레이션|CHF 상관식|Boiling
+CHF mechanism|/siː eɪtʃ ɛf ˈmɛkənɪzəm/|씨에이치에프 메커니즘|CHF 메커니즘|Boiling
+bubble interference|/ˈbʌbəl ˌɪntərˈfɪrəns/|버블 인터피어런스|기포 간섭|Boiling
+hot-dry spot model|/hɑːt draɪ spɑːt ˈmɑːdəl/|핫 드라이 스팟 모델|고온-건조점 모델|Boiling
+macrolayer evaporation model|/ˈmækroʊleɪər ɪˌvæpəˈreɪʃən ˈmɑːdəl/|매크로레이어 이배퍼레이션 모델|매크로층 증발 모델|Boiling
+hydrodynamic model|/ˌhaɪdroʊdaɪˈnæmɪk ˈmɑːdəl/|하이드로다이내믹 모델|유체역학 모델|Boiling
+liquid-vapor interface|/ˈlɪkwɪd ˈveɪpər ˈɪntərfeɪs/|리퀴드 베이퍼 인터페이스|액체-증기 계면|Bubble Dynamics
+interfacial curvature|/ˌɪntərˈfeɪʃəl ˈkɜːrvətʃər/|인터페이셜 커버처|계면 곡률|Bubble Dynamics
+interfacial velocity|/ˌɪntərˈfeɪʃəl vəˈlɑːsəti/|인터페이셜 벌라서티|계면 속도|Bubble Dynamics
+interfacial instability|/ˌɪntərˈfeɪʃəl ˌɪnstəˈbɪləti/|인터페이셜 인스터빌리티|계면 불안정성|Bubble Dynamics
+vapor momentum|/ˈveɪpər moʊˈmɛntəm/|베이퍼 모멘텀|증기 운동량|Bubble Dynamics
+pressure force|/ˈprɛʃər fɔːrs/|프레셔 포스|압력력|Bubble Dynamics
+surface-tension force|/ˈsɜːrfɪs ˈtɛnʃən fɔːrs/|서피스 텐션 포스|표면장력 힘|Bubble Dynamics
+buoyancy force|/ˈbɔɪənsi fɔːrs/|보이언시 포스|부력|Bubble Dynamics
+force balance|/fɔːrs ˈbæləns/|포스 밸런스|힘 평형|Bubble Dynamics
+bubble shape|/ˈbʌbəl ʃeɪp/|버블 셰이프|기포 형상|Bubble Dynamics
+mushroom-shaped bubble|/ˈmʌʃruːm ʃeɪpt ˈbʌbəl/|머쉬룸 셰이프트 버블|버섯형 기포|Bubble Dynamics
+hovering bubble|/ˈhʌvərɪŋ ˈbʌbəl/|호버링 버블|부유 기포|Bubble Dynamics
+bubble plume|/ˈbʌbəl pluːm/|버블 플룸|기포 플룸|Bubble Dynamics
+bubble cluster|/ˈbʌbəl ˈklʌstər/|버블 클러스터|기포 군집|Bubble Dynamics
+bubble bundle|/ˈbʌbəl ˈbʌndəl/|버블 번들|기포 다발|Bubble Dynamics
+vapor column|/ˈveɪpər ˈkɑːləm/|베이퍼 칼럼|증기 기둥|Bubble Dynamics
+bubble release|/ˈbʌbəl rɪˈliːs/|버블 릴리스|기포 방출|Bubble Dynamics
+bubble rise velocity|/ˈbʌbəl raɪz vəˈlɑːsəti/|버블 라이즈 벌라서티|기포 상승속도|Bubble Dynamics
+bubble growth rate|/ˈbʌbəl ɡroʊθ reɪt/|버블 그로스 레이트|기포 성장률|Bubble Dynamics
+bubble lifetime|/ˈbʌbəl ˈlaɪftaɪm/|버블 라이프타임|기포 수명|Bubble Dynamics
+bubble period|/ˈbʌbəl ˈpɪriəd/|버블 피리어드|기포 주기|Bubble Dynamics
+nucleation frequency|/ˌnuːkliˈeɪʃən ˈfriːkwənsi/|뉴클리에이션 프리퀀시|핵생성 빈도|Bubble Dynamics
+site activation|/saɪt ˌæktəˈveɪʃən/|사이트 액티베이션|핵생성 지점 활성화|Bubble Dynamics
+cavity activation|/ˈkævəti ˌæktəˈveɪʃən/|캐비티 액티베이션|공동 활성화|Bubble Dynamics
+departure frequency|/dɪˈpɑːrtʃər ˈfriːkwənsi/|디파처 프리퀀시|이탈 빈도|Bubble Dynamics
+bubble detachment|/ˈbʌbəl dɪˈtætʃmənt/|버블 디태치먼트|기포 이탈|Bubble Dynamics
+liquid inflow|/ˈlɪkwɪd ˈɪnfloʊ/|리퀴드 인플로|액체 유입|Bubble Dynamics
+vapor outflow|/ˈveɪpər ˈaʊtfloʊ/|베이퍼 아웃플로|증기 유출|Bubble Dynamics
+radial liquid inflow|/ˈreɪdiəl ˈlɪkwɪd ˈɪnfloʊ/|레이디얼 리퀴드 인플로|반경방향 액체 유입|Bubble Dynamics
+liquid supply|/ˈlɪkwɪd səˈplaɪ/|리퀴드 서플라이|액체 공급|Bubble Dynamics
+surface cooling|/ˈsɜːrfɪs ˈkuːlɪŋ/|서피스 쿨링|표면 냉각|Heat Transfer
+rewetting front|/ˌriːˈwɛtɪŋ frʌnt/|리웨팅 프런트|재젖음 전선|Bubble Dynamics`);
