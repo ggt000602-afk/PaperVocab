bdc1445125345aaa8580597b847fcbd63aa8cd8b
ggt000602-afk <ggt000602@gmail.com>
2026-08-12 14:27:29 +0900

    Add high-frequency academic reading vocabulary part 4

---
+++ data_more4.js
@@ -0,0 +1,107 @@
+addBulkWords(`approach|/əˈproʊtʃ/|어프로치|접근법|Academic English
+method|/ˈmɛθəd/|메서드|방법|Academic English
+procedure|/prəˈsiːdʒər/|프로시저|절차|Academic English
+technique|/tɛkˈniːk/|테크닉|기법|Academic English
+model|/ˈmɑːdəl/|모델|모델|Academic English
+theory|/ˈθɪəri/|씨어리|이론|Academic English
+formulation|/ˌfɔːrmjəˈleɪʃən/|포뮬레이션|정식화, 수식화|Academic English
+framework|/ˈfreɪmwɜːrk/|프레임워크|분석 틀|Academic English
+concept|/ˈkɑːnsɛpt/|콘셉트|개념|Academic English
+principle|/ˈprɪnsəpəl/|프린서플|원리|Academic English
+factor|/ˈfæktər/|팩터|요인|Academic English
+variable|/ˈvɛriəbəl/|베리어블|변수|Academic English
+quantity|/ˈkwɑːntəti/|콴터티|물리량, 양|Academic English
+value|/ˈvæljuː/|밸류|값|Academic English
+range|/reɪndʒ/|레인지|범위|Academic English
+condition|/kənˈdɪʃən/|컨디션|조건|Academic English
+case|/keɪs/|케이스|경우, 사례|Academic English
+regime|/reɪˈʒiːm/|레짐|영역, 지배 상태|Academic English
+region|/ˈriːdʒən/|리전|영역|Academic English
+state|/steɪt/|스테이트|상태|Academic English
+stage|/steɪdʒ/|스테이지|단계|Academic English
+process|/ˈprɑːsɛs/|프로세스|과정|Academic English
+sequence|/ˈsiːkwəns/|시퀀스|순서, 연속 과정|Academic English
+cycle|/ˈsaɪkəl/|사이클|주기|Academic English
+period|/ˈpɪriəd/|피리어드|기간, 주기|Academic English
+rate|/reɪt/|레이트|율, 속도|Academic English
+frequency|/ˈfriːkwənsi/|프리퀀시|빈도, 주파수|Academic English
+ratio|/ˈreɪʃioʊ/|레이쇼|비율|Academic English
+fraction|/ˈfrækʃən/|프랙션|분율|Academic English
+difference|/ˈdɪfərəns/|디퍼런스|차이|Academic English
+similarity|/ˌsɪməˈlærəti/|시밀래러티|유사성|Academic English
+comparison|/kəmˈpærɪsən/|컴패리슨|비교|Academic English
+agreement|/əˈɡriːmənt/|어그리먼트|일치|Academic English
+prediction|/prɪˈdɪkʃən/|프리딕션|예측|Academic English
+measurement|/ˈmɛʒərmənt/|메저먼트|측정|Academic English
+observation|/ˌɑːbzərˈveɪʃən/|압저베이션|관찰|Academic English
+analysis|/əˈnæləsɪs/|어낼러시스|분석|Academic English
+calculation|/ˌkælkjəˈleɪʃən/|캘큘레이션|계산|Academic English
+simulation|/ˌsɪmjəˈleɪʃən/|시뮬레이션|모사|Academic English
+experiment|/ɪkˈspɛrəmənt/|익스페러먼트|실험|Academic English
+investigation|/ɪnˌvɛstəˈɡeɪʃən/|인베스터게이션|조사, 연구|Academic English
+study|/ˈstʌdi/|스터디|연구|Academic English
+finding|/ˈfaɪndɪŋ/|파인딩|연구 결과|Academic English
+result|/rɪˈzʌlt/|리절트|결과|Academic English
+conclusion|/kənˈkluːʒən/|컨클루전|결론|Academic English
+evidence|/ˈɛvɪdəns/|에비던스|근거, 증거|Academic English
+data|/ˈdeɪtə/|데이터|데이터|Academic English
+literature|/ˈlɪtərətʃər/|리터러처|문헌|Academic English
+reference|/ˈrɛfərəns/|레퍼런스|참고문헌, 기준|Academic English
+previous study|/ˈpriːviəs ˈstʌdi/|프리비어스 스터디|선행연구|Academic English
+present study|/ˈprɛzənt ˈstʌdi/|프레전트 스터디|본 연구|Academic English
+experimental data|/ɪkˌspɛrəˈmɛntəl ˈdeɪtə/|익스페리멘털 데이터|실험 데이터|Academic English
+theoretical prediction|/ˌθiːəˈrɛtɪkəl prɪˈdɪkʃən/|씨어레티컬 프리딕션|이론 예측|Academic English
+numerical result|/nuːˈmɛrɪkəl rɪˈzʌlt/|뉴메리컬 리절트|수치 결과|Academic English
+experimental result|/ɪkˌspɛrəˈmɛntəl rɪˈzʌlt/|익스페리멘털 리절트|실험 결과|Academic English
+boundary condition|/ˈbaʊndəri kənˈdɪʃən/|바운더리 컨디션|경계조건|Academic English
+initial condition|/ɪˈnɪʃəl kənˈdɪʃən/|이니셜 컨디션|초기조건|Academic English
+operating condition|/ˈɑːpəreɪtɪŋ kənˈdɪʃən/|오퍼레이팅 컨디션|운전조건|Academic English
+test condition|/tɛst kənˈdɪʃən/|테스트 컨디션|시험조건|Academic English
+ambient condition|/ˈæmbiənt kənˈdɪʃən/|앰비언트 컨디션|주변조건|Academic English
+experimental condition|/ɪkˌspɛrəˈmɛntəl kənˈdɪʃən/|익스페리멘털 컨디션|실험조건|Academic English
+parameter range|/pəˈræmɪtər reɪndʒ/|퍼래미터 레인지|변수 범위|Academic English
+valid range|/ˈvælɪd reɪndʒ/|밸리드 레인지|유효 범위|Academic English
+order of magnitude|/ˈɔːrdər əv ˈmæɡnɪtuːd/|오더 오브 매그니튜드|자릿수 규모|Academic English
+upper limit|/ˈʌpər ˈlɪmɪt/|어퍼 리밋|상한|Academic English
+lower limit|/ˈloʊər ˈlɪmɪt/|로어 리밋|하한|Academic English
+maximum value|/ˈmæksɪməm ˈvæljuː/|맥시멈 밸류|최대값|Academic English
+minimum value|/ˈmɪnɪməm ˈvæljuː/|미니멈 밸류|최소값|Academic English
+average value|/ˈævərɪdʒ ˈvæljuː/|애버리지 밸류|평균값|Academic English
+peak value|/piːk ˈvæljuː/|피크 밸류|첨두값|Academic English
+local value|/ˈloʊkəl ˈvæljuː/|로컬 밸류|국부값|Academic English
+overall|/ˌoʊvərˈɔːl/|오버롤|전체적인|Academic English
+primarily|/praɪˈmɛrəli/|프라이메럴리|주로|Academic English
+mainly|/ˈmeɪnli/|메인리|주로|Academic English
+partly|/ˈpɑːrtli/|파트리|부분적으로|Academic English
+largely|/ˈlɑːrdʒli/|라즐리|대체로, 크게|Academic English
+strongly|/ˈstrɔːŋli/|스트롱리|강하게|Academic English
+slightly|/ˈslaɪtli/|슬라이트리|약간|Academic English
+gradually|/ˈɡrædʒuəli/|그래주얼리|점진적으로|Academic English
+rapidly|/ˈræpɪdli/|래피들리|빠르게|Academic English
+sharply|/ˈʃɑːrpli/|샤플리|급격하게|Academic English
+nearly|/ˈnɪrli/|니얼리|거의|Academic English
+roughly|/ˈrʌfli/|러플리|대략|Academic English
+typically|/ˈtɪpɪkli/|티피컬리|일반적으로, 전형적으로|Academic English
+generally|/ˈdʒɛnərəli/|제너럴리|일반적으로|Academic English
+particularly|/pərˈtɪkjələrli/|퍼티큘럴리|특히|Academic English
+specifically|/spəˈsɪfɪkli/|스페시피컬리|구체적으로|Academic English
+essentially|/ɪˈsɛnʃəli/|에센셜리|본질적으로|Academic English
+apparently|/əˈpærəntli/|어패런틀리|겉보기에는|Academic English
+experimentally|/ɪkˌspɛrəˈmɛntəli/|익스페리멘털리|실험적으로|Academic English
+theoretically|/ˌθiːəˈrɛtɪkli/|씨어레티컬리|이론적으로|Academic English
+numerically|/nuːˈmɛrɪkli/|뉴메리컬리|수치적으로|Academic English
+quantitatively|/ˈkwɑːntəˌteɪtɪvli/|콴터테이티블리|정량적으로|Academic English
+qualitatively|/ˈkwɑːləˌteɪtɪvli/|콸러테이티블리|정성적으로|Academic English
+independently|/ˌɪndɪˈpɛndəntli/|인디펜던틀리|독립적으로|Academic English
+simultaneously|/ˌsaɪməlˈteɪniəsli/|사이멀테이니어슬리|동시에|Academic English
+continuously|/kənˈtɪnjuəsli/|컨티뉴어슬리|연속적으로|Academic English
+periodically|/ˌpɪriˈɑːdɪkli/|피리오디컬리|주기적으로|Academic English
+initially|/ɪˈnɪʃəli/|이니셜리|초기에|Academic English
+subsequently|/ˈsʌbsɪkwəntli/|섭시퀀틀리|그 후에|Academic English
+finally|/ˈfaɪnəli/|파이널리|마지막으로|Academic English
+respect to|/rɪˈspɛkt tuː/|리스펙트 투|~에 관한|Academic English
+in the case of|/ɪn ðə keɪs əv/|인 더 케이스 오브|~의 경우|Academic English
+for example|/fɔːr ɪɡˈzæmpəl/|포 이그잼플|예를 들어|Academic English
+for instance|/fɔːr ˈɪnstəns/|포 인스턴스|예를 들어|Academic English
+in particular|/ɪn pərˈtɪkjələr/|인 퍼티큘러|특히|Academic English
+namely|/ˈneɪmli/|네임리|즉, 구체적으로|Academic English`);
