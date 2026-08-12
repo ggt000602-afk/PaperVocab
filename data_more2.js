0d882cbdaa12b5cf0a2da9e072f73a14364c2ca6
ggt000602-afk <ggt000602@gmail.com>
2026-08-12 14:19:49 +0900

    Add academic and technical vocabulary expansion part 2

---
+++ data_more2.js
@@ -0,0 +1,99 @@
+addBulkWords(`correct|/kəˈrɛkt/|커렉트|보정하다|Academic English
+reject|/rɪˈdʒɛkt/|리젝트|제외하다, 거부하다|Academic English
+reproduce|/ˌriːprəˈduːs/|리프로듀스|재현하다|Academic English
+immerse|/ɪˈmɜːrs/|이머스|침지하다|Academic English
+mount|/maʊnt/|마운트|장착하다|Academic English
+embed|/ɪmˈbɛd/|임베드|매립하다|Academic English
+insulate|/ˈɪnsəleɪt/|인슐레이트|단열하다|Academic English
+fabricate|/ˈfæbrɪkeɪt/|패브리케이트|제작하다|Academic English
+nevertheless|/ˌnɛvərðəˈlɛs/|네버더레스|그럼에도 불구하고|Academic English
+moreover|/mɔːrˈoʊvər/|모어오버|더욱이|Academic English
+furthermore|/ˈfɜːrðərmɔːr/|퍼더모어|더 나아가|Academic English
+consequently|/ˈkɑːnsəkwəntli/|콘시퀀틀리|결과적으로|Academic English
+hence|/hɛns/|헨스|따라서|Academic English
+thus|/ðʌs/|더스|따라서|Academic English
+meanwhile|/ˈmiːnwaɪl/|민와일|한편|Academic English
+similarly|/ˈsɪmələrli/|시밀러리|유사하게|Academic English
+conversely|/ˈkɑːnvɜːrsli/|컨버슬리|반대로|Academic English
+in contrast|/ɪn ˈkɑːntræst/|인 콘트래스트|대조적으로|Academic English
+on the other hand|/ɑːn ði ˈʌðər hænd/|온 디 아더 핸드|반면에|Academic English
+in addition|/ɪn əˈdɪʃən/|인 어디션|추가로|Academic English
+as a result|/æz ə rɪˈzʌlt/|애즈 어 리절트|그 결과|Academic English
+with respect to|/wɪð rɪˈspɛkt tuː/|위드 리스펙트 투|~에 관하여|Academic English
+with regard to|/wɪð rɪˈɡɑːrd tuː/|위드 리가드 투|~에 관하여|Academic English
+as a function of|/æz ə ˈfʌŋkʃən əv/|애즈 어 펑션 오브|~의 함수로|Academic English
+in accordance with|/ɪn əˈkɔːrdəns wɪð/|인 어코던스 위드|~에 따라|Academic English
+in comparison with|/ɪn kəmˈpærɪsən wɪð/|인 컴패리슨 위드|~와 비교하여|Academic English
+be characterized by|/biː ˈkærəktəraɪzd baɪ/|비 캐릭터라이즈드 바이|~로 특징지어지다|Academic English
+be governed by|/biː ˈɡʌvərnd baɪ/|비 거번드 바이|~에 의해 지배되다|Academic English
+be associated with|/biː əˈsoʊsieɪtɪd wɪð/|비 어소시에이티드 위드|~와 관련되다|Academic English
+be accompanied by|/biː əˈkʌmpənid baɪ/|비 어컴퍼니드 바이|~을 동반하다|Academic English
+be considered as|/biː kənˈsɪdərd æz/|비 컨시더드 애즈|~로 간주되다|Academic English
+be regarded as|/biː rɪˈɡɑːrdɪd æz/|비 리가디드 애즈|~로 여겨지다|Academic English
+be defined as|/biː dɪˈfaɪnd æz/|비 디파인드 애즈|~로 정의되다|Academic English
+be expressed as|/biː ɪkˈsprɛst æz/|비 익스프레스트 애즈|~로 표현되다|Academic English
+be derived from|/biː dɪˈraɪvd frəm/|비 디라이브드 프롬|~로부터 유도되다|Academic English
+be subjected to|/biː səbˈdʒɛktɪd tuː/|비 섭젝티드 투|~에 노출·가해지다|Academic English
+be immersed in|/biː ɪˈmɜːrst ɪn/|비 이머스트 인|~에 잠기다|Academic English
+be exposed to|/biː ɪkˈspoʊzd tuː/|비 익스포즈드 투|~에 노출되다|Academic English
+be maintained at|/biː meɪnˈteɪnd æt/|비 메인테인드 앳|~로 유지되다|Academic English
+be kept constant|/biː kɛpt ˈkɑːnstənt/|비 켑트 콘스턴트|일정하게 유지되다|Academic English
+standard deviation|/ˈstændərd ˌdiːviˈeɪʃən/|스탠더드 디비에이션|표준편차|Data & Statistics
+standard error|/ˈstændərd ˈɛrər/|스탠더드 에러|표준오차|Data & Statistics
+confidence interval|/ˈkɑːnfɪdəns ˈɪntərvəl/|컨피던스 인터벌|신뢰구간|Data & Statistics
+error bar|/ˈɛrər bɑːr/|에러 바|오차막대|Data & Statistics
+relative error|/ˈrɛlətɪv ˈɛrər/|렐러티브 에러|상대오차|Data & Statistics
+absolute error|/ˈæbsəluːt ˈɛrər/|앱솔루트 에러|절대오차|Data & Statistics
+error propagation|/ˈɛrər ˌprɑːpəˈɡeɪʃən/|에러 프로퍼게이션|오차 전파|Data & Statistics
+regression|/rɪˈɡrɛʃən/|리그레션|회귀|Data & Statistics
+linear regression|/ˈlɪniər rɪˈɡrɛʃən/|리니어 리그레션|선형회귀|Data & Statistics
+least-squares fit|/liːst skwɛrz fɪt/|리스트 스퀘어즈 핏|최소제곱 적합|Data & Statistics
+coefficient of determination|/ˌkoʊɪˈfɪʃənt əv dɪˌtɜːrmɪˈneɪʃən/|코이피션트 오브 디터미네이션|결정계수|Data & Statistics
+residual|/rɪˈzɪdʒuəl/|리지듀얼|잔차|Data & Statistics
+outlier|/ˈaʊtlaɪər/|아웃라이어|이상치|Data & Statistics
+scatter|/ˈskætər/|스캐터|산포|Data & Statistics
+data point|/ˈdeɪtə pɔɪnt/|데이터 포인트|데이터 점|Data & Statistics
+data set|/ˈdeɪtə sɛt/|데이터 세트|데이터 세트|Data & Statistics
+sample size|/ˈsæmpəl saɪz/|샘플 사이즈|표본 크기|Data & Statistics
+mean value|/miːn ˈvæljuː/|민 밸류|평균값|Data & Statistics
+median|/ˈmiːdiən/|미디언|중앙값|Data & Statistics
+variance|/ˈvɛriəns/|베리언스|분산|Data & Statistics
+sensitivity analysis|/ˌsɛnsəˈtɪvəti əˈnæləsɪs/|센서티비티 어낼러시스|민감도 분석|Data & Statistics
+dimensional analysis|/dɪˈmɛnʃənəl əˈnæləsɪs/|디멘셔널 어낼러시스|차원해석|Data & Statistics
+dimensionless number|/dɪˈmɛnʃənləs ˈnʌmbər/|디멘션리스 넘버|무차원수|Data & Statistics
+normalized value|/ˈnɔːrməlaɪzd ˈvæljuː/|노멀라이즈드 밸류|정규화 값|Data & Statistics
+baseline|/ˈbeɪslaɪn/|베이스라인|기준선, 기준조건|Data & Statistics
+reference value|/ˈrɛfərəns ˈvæljuː/|레퍼런스 밸류|기준값|Data & Statistics
+relative change|/ˈrɛlətɪv tʃeɪndʒ/|렐러티브 체인지|상대 변화|Data & Statistics
+percentage increase|/pərˈsɛntɪdʒ ˈɪnkriːs/|퍼센티지 인크리스|백분율 증가|Data & Statistics
+hysteresis|/ˌhɪstəˈriːsɪs/|히스테리시스|이력현상|Data & Statistics
+sensible heat|/ˈsɛnsəbəl hiːt/|센서블 히트|현열|Heat Transfer
+heat transfer rate|/hiːt ˈtrænsfər reɪt/|히트 트랜스퍼 레이트|열전달률|Heat Transfer
+heat transfer mechanism|/hiːt ˈtrænsfər ˈmɛkənɪzəm/|히트 트랜스퍼 메커니즘|열전달 메커니즘|Heat Transfer
+conduction|/kənˈdʌkʃən/|컨덕션|전도|Heat Transfer
+convection|/kənˈvɛkʃən/|컨벡션|대류|Heat Transfer
+radiation|/ˌreɪdiˈeɪʃən/|레이디에이션|복사|Heat Transfer
+boiling heat transfer|/ˈbɔɪlɪŋ hiːt ˈtrænsfər/|보일링 히트 트랜스퍼|비등 열전달|Heat Transfer
+forced convection|/fɔːrst kənˈvɛkʃən/|포스드 컨벡션|강제대류|Heat Transfer
+two-phase flow|/tuː feɪz floʊ/|투 페이즈 플로|2상 유동|Heat Transfer
+vapor generation|/ˈveɪpər ˌdʒɛnəˈreɪʃən/|베이퍼 제너레이션|증기 생성|Boiling
+vapor production|/ˈveɪpər prəˈdʌkʃən/|베이퍼 프로덕션|증기 발생|Boiling
+vapor jet|/ˈveɪpər dʒɛt/|베이퍼 젯|증기 제트|Bubble Dynamics
+vapor stem|/ˈveɪpər stɛm/|베이퍼 스템|증기 기둥|Bubble Dynamics
+vapor mushroom|/ˈveɪpər ˈmʌʃruːm/|베이퍼 머쉬룸|버섯형 증기 구조|Bubble Dynamics
+vapor mass|/ˈveɪpər mæs/|베이퍼 매스|증기 덩어리|Bubble Dynamics
+bubble waiting time|/ˈbʌbəl ˈweɪtɪŋ taɪm/|버블 웨이팅 타임|기포 대기시간|Bubble Dynamics
+waiting period|/ˈweɪtɪŋ ˈpɪriəd/|웨이팅 피리어드|대기 주기|Bubble Dynamics
+bubble collapse|/ˈbʌbəl kəˈlæps/|버블 컬랩스|기포 붕괴|Bubble Dynamics
+condensation|/ˌkɑːndənˈseɪʃən/|콘덴세이션|응축|Heat Transfer
+vaporization|/ˌveɪpərəˈzeɪʃən/|베이퍼리제이션|기화|Heat Transfer
+nucleation density|/ˌnuːkliˈeɪʃən ˈdɛnsəti/|뉴클리에이션 덴서티|핵생성 밀도|Bubble Dynamics
+heterogeneous nucleation|/ˌhɛtərəˈdʒiːniəs ˌnuːkliˈeɪʃən/|헤터러지니어스 뉴클리에이션|불균일 핵생성|Bubble Dynamics
+homogeneous nucleation|/ˌhoʊməˈdʒiːniəs ˌnuːkliˈeɪʃən/|호머지니어스 뉴클리에이션|균일 핵생성|Bubble Dynamics
+vapor embryo|/ˈveɪpər ˈɛmbrioʊ/|베이퍼 엠브리오|증기 배아|Bubble Dynamics
+trapped vapor|/træpt ˈveɪpər/|트랩트 베이퍼|포획 증기|Bubble Dynamics
+superheated liquid|/ˌsuːpərˈhiːtɪd ˈlɪkwɪd/|슈퍼히티드 리퀴드|과열 액체|Boiling
+vapor quality|/ˈveɪpər ˈkwɑːləti/|베이퍼 퀄리티|증기 건도|Heat Transfer
+liquid film|/ˈlɪkwɪd fɪlm/|리퀴드 필름|액막|Bubble Dynamics
+liquid sublayer|/ˈlɪkwɪd ˈsʌbleɪər/|리퀴드 서브레이어|액체 하부층|Bubble Dynamics
+macrolayer dryout|/ˈmækroʊleɪər ˈdraɪaʊt/|매크로레이어 드라이아웃|매크로층 건조|Boiling`);
