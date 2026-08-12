11abf106a509e234bf220447e6f2eefc11c8c16c
ggt000602-afk <ggt000602@gmail.com>
2026-08-12 14:29:06 +0900

    Add experimental methods and paper-structure vocabulary part 6

---
+++ data_more6.js
@@ -0,0 +1,106 @@
+addBulkWords(`abstract|/ˈæbstrækt/|앱스트랙트|초록|Academic English
+introduction|/ˌɪntrəˈdʌkʃən/|인트러덕션|서론|Academic English
+methodology|/ˌmɛθəˈdɑːlədʒi/|메서돌러지|방법론|Academic English
+materials and methods|/məˈtɪriəlz ænd ˈmɛθədz/|머티리얼즈 앤드 메서즈|재료 및 방법|Academic English
+results and discussion|/rɪˈzʌlts ænd dɪˈskʌʃən/|리절츠 앤드 디스커션|결과 및 고찰|Academic English
+concluding remarks|/kənˈkluːdɪŋ rɪˈmɑːrks/|컨클루딩 리마크스|결론적 논평|Academic English
+nomenclature|/ˈnoʊmənˌkleɪtʃər/|노먼클레이처|기호 설명|Academic English
+appendix|/əˈpɛndɪks/|어펜딕스|부록|Academic English
+acknowledgment|/əkˈnɑːlɪdʒmənt/|액날리지먼트|감사의 글|Academic English
+figure|/ˈfɪɡjər/|피겨|그림|Academic English
+table|/ˈteɪbəl/|테이블|표|Academic English
+caption|/ˈkæpʃən/|캡션|그림 설명|Academic English
+schematic|/skiˈmætɪk/|스키매틱|개략도|Academic English
+illustration|/ˌɪləˈstreɪʃən/|일러스트레이션|도해|Academic English
+cross-section|/ˈkrɔːs sɛkʃən/|크로스 섹션|단면|Academic English
+as shown in|/æz ʃoʊn ɪn/|애즈 쇼운 인|~에 나타낸 바와 같이|Academic English
+as illustrated in|/æz ˈɪləstreɪtɪd ɪn/|애즈 일러스트레이티드 인|~에 도시한 바와 같이|Academic English
+as listed in|/æz ˈlɪstɪd ɪn/|애즈 리스티드 인|~에 열거한 바와 같이|Academic English
+as summarized in|/æz ˈsʌməraɪzd ɪn/|애즈 서머라이즈드 인|~에 요약한 바와 같이|Academic English
+as described above|/æz dɪˈskraɪbd əˈbʌv/|애즈 디스크라이브드 어버브|위에서 설명한 바와 같이|Academic English
+as follows|/æz ˈfɑːloʊz/|애즈 팔로우즈|다음과 같이|Academic English
+given by|/ˈɡɪvən baɪ/|기븐 바이|~로 주어지다|Academic English
+where|/wɛr/|웨어|여기서|Academic English
+respectively given by|/rɪˈspɛktɪvli ˈɡɪvən baɪ/|리스펙티블리 기븐 바이|각각 ~로 주어지다|Academic English
+corresponding to|/ˌkɔːrəˈspɑːndɪŋ tuː/|코러스판딩 투|~에 해당하는|Academic English
+increasing with|/ɪnˈkriːsɪŋ wɪð/|인크리싱 위드|~에 따라 증가하는|Academic English
+decreasing with|/dɪˈkriːsɪŋ wɪð/|디크리싱 위드|~에 따라 감소하는|Academic English
+proportional to|/prəˈpɔːrʃənəl tuː/|프로포셔널 투|~에 비례하는|Academic English
+inversely proportional to|/ɪnˈvɜːrsli prəˈpɔːrʃənəl tuː/|인버슬리 프로포셔널 투|~에 반비례하는|Academic English
+regardless of|/rɪˈɡɑːrdləs əv/|리가들리스 오브|~와 상관없이|Academic English
+despite|/dɪˈspaɪt/|디스파이트|~에도 불구하고|Academic English
+although|/ɔːlˈðoʊ/|올도우|비록 ~이지만|Academic English
+while|/waɪl/|와일|~하는 동안, 반면|Academic English
+since|/sɪns/|신스|~이므로, ~이후|Academic English
+because of|/bɪˈkɔːz əv/|비코즈 오브|~ 때문에|Academic English
+owing to|/ˈoʊɪŋ tuː/|오잉 투|~ 때문에|Academic English
+thereby|/ˌðɛrˈbaɪ/|데어바이|그렇게 함으로써|Academic English
+whereby|/ˌwɛrˈbaɪ/|웨어바이|그로써, 그에 의해|Academic English
+therein|/ˌðɛrˈɪn/|데어린|그 안에|Academic English
+thereof|/ˌðɛrˈʌv/|데어러브|그것의|Academic English
+herein|/ˌhɪrˈɪn/|히어린|본 논문에서|Academic English
+above-mentioned|/əˈbʌv ˈmɛnʃənd/|어버브 멘션드|상기한|Academic English
+aforementioned|/əˌfɔːrˈmɛnʃənd/|어포어멘션드|앞서 언급한|Academic English
+correspondingly|/ˌkɔːrəˈspɑːndɪŋli/|코러스판딩리|그에 상응하여|Academic English
+alternatively|/ɔːlˈtɜːrnətɪvli/|얼터너티블리|대안으로|Academic English
+otherwise|/ˈʌðərwaɪz/|아더와이즈|그렇지 않으면|Academic English
+presumably|/prɪˈzuːməbli/|프리주머블리|추정컨대|Academic English
+possibly|/ˈpɑːsəbli/|파서블리|가능하게는|Academic English
+likely|/ˈlaɪkli/|라이클리|가능성이 높은|Academic English
+unlikely|/ʌnˈlaɪkli/|언라이클리|가능성이 낮은|Academic English
+potentially|/pəˈtɛnʃəli/|퍼텐셜리|잠재적으로|Academic English
+approximately constant|/əˈprɑːksɪmətli ˈkɑːnstənt/|어프락시멋리 콘스턴트|거의 일정한|Academic English
+nearly constant|/ˈnɪrli ˈkɑːnstənt/|니얼리 콘스턴트|거의 일정한|Academic English
+monotonically|/ˌmɑːnəˈtɑːnɪkli/|모너토니컬리|단조롭게|Academic English
+nonlinear|/ˌnɑːnˈlɪniər/|논리니어|비선형의|Academic English
+linear|/ˈlɪniər/|리니어|선형의|Academic English
+asymptotic|/ˌæsɪmˈtɑːtɪk/|애심타틱|점근적인|Academic English
+steady|/ˈstɛdi/|스테디|정상적인, 일정한|Academic English
+unsteady|/ʌnˈstɛdi/|언스테디|비정상적인|Academic English
+stable|/ˈsteɪbəl/|스테이블|안정한|Academic English
+unstable|/ʌnˈsteɪbəl/|언스테이블|불안정한|Academic English
+random|/ˈrændəm/|랜덤|무작위의|Academic English
+repeatable|/rɪˈpiːtəbəl/|리피터블|반복 가능한|Academic English
+reproducible|/ˌriːprəˈduːsəbəl/|리프로듀서블|재현 가능한|Academic English
+sensor|/ˈsɛnsər/|센서|센서|Experiment
+transducer|/trænzˈduːsər/|트랜스듀서|변환기|Experiment
+DAQ|/diː eɪ kjuː/|디에이큐|데이터 획득장치|Experiment
+signal|/ˈsɪɡnəl/|시그널|신호|Experiment
+noise|/nɔɪz/|노이즈|잡음|Experiment
+measurement range|/ˈmɛʒərmənt reɪndʒ/|메저먼트 레인지|측정 범위|Experiment
+full scale|/fʊl skeɪl/|풀 스케일|풀스케일|Experiment
+resolution|/ˌrɛzəˈluːʃən/|레졸루션|분해능|Experiment
+response time|/rɪˈspɑːns taɪm/|리스판스 타임|응답시간|Experiment
+time constant|/taɪm ˈkɑːnstənt/|타임 콘스턴트|시정수|Experiment
+sensor drift|/ˈsɛnsər drɪft/|센서 드리프트|센서 드리프트|Experiment
+offset|/ˈɔːfsɛt/|오프셋|오프셋|Experiment
+zero offset|/ˈzɪroʊ ˈɔːfsɛt/|제로 오프셋|영점 오프셋|Experiment
+calibration curve|/ˌkælɪˈbreɪʃən kɜːrv/|캘리브레이션 커브|교정곡선|Experiment
+calibration point|/ˌkælɪˈbreɪʃən pɔɪnt/|캘리브레이션 포인트|교정점|Experiment
+calibration error|/ˌkælɪˈbreɪʃən ˈɛrər/|캘리브레이션 에러|교정오차|Experiment
+instrument uncertainty|/ˈɪnstrəmənt ʌnˈsɜːrtənti/|인스트루먼트 언서턴티|계측기 불확도|Experiment
+combined uncertainty|/kəmˈbaɪnd ʌnˈsɜːrtənti/|컴바인드 언서턴티|합성불확도|Experiment
+expanded uncertainty|/ɪkˈspændɪd ʌnˈsɜːrtənti/|익스팬디드 언서턴티|확장불확도|Experiment
+coverage factor|/ˈkʌvərɪdʒ ˈfæktər/|커버리지 팩터|포함인자|Experiment
+bias|/ˈbaɪəs/|바이어스|편향|Experiment
+random error|/ˈrændəm ˈɛrər/|랜덤 에러|우연오차|Experiment
+measurement error|/ˈmɛʒərmənt ˈɛrər/|메저먼트 에러|측정오차|Experiment
+uncertainty budget|/ʌnˈsɜːrtənti ˈbʌdʒɪt/|언서턴티 버짓|불확도 예산|Experiment
+traceability|/ˌtreɪsəˈbɪləti/|트레이서빌리티|측정 소급성|Experiment
+reference instrument|/ˈrɛfərəns ˈɪnstrəmənt/|레퍼런스 인스트루먼트|기준 계측기|Experiment
+standard platinum resistance thermometer|/ˈstændərd ˈplætɪnəm rɪˈzɪstəns θərˈmɑːmətər/|스탠더드 플래티넘 리지스턴스 써모미터|표준 백금저항온도계|Experiment
+vernier caliper|/ˈvɜːrniər ˈkælɪpər/|버니어 캘리퍼|버니어 캘리퍼스|Experiment
+micrometer|/maɪˈkrɑːmətər/|마이크로미터|마이크로미터|Experiment
+oscilloscope|/əˈsɪləskoʊp/|어실러스코프|오실로스코프|Experiment
+multimeter|/ˈmʌltiˌmiːtər/|멀티미터|멀티미터|Experiment
+voltmeter|/ˈvoʊltˌmiːtər/|볼트미터|전압계|Experiment
+ammeter|/ˈæmˌmiːtər/|앰미터|전류계|Experiment
+wiring|/ˈwaɪərɪŋ/|와이어링|배선|Experiment
+lead resistance|/liːd rɪˈzɪstəns/|리드 리지스턴스|리드선 저항|Experiment
+contact voltage|/ˈkɑːntækt ˈvoʊltɪdʒ/|콘택트 볼티지|접촉 전압|Experiment
+electrical noise|/ɪˈlɛktrɪkəl nɔɪz/|일렉트리컬 노이즈|전기적 잡음|Experiment
+low-pass filter|/loʊ pæs ˈfɪltər/|로우 패스 필터|저역통과필터|Experiment
+moving average|/ˈmuːvɪŋ ˈævərɪdʒ/|무빙 애버리지|이동평균|Data & Statistics
+filtering|/ˈfɪltərɪŋ/|필터링|필터링|Data & Statistics
+smoothing|/ˈsmuːðɪŋ/|스무딩|평활화|Data & Statistics
+signal processing|/ˈsɪɡnəl ˈprɑːsɛsɪŋ/|시그널 프로세싱|신호처리|Data & Statistics`);
