bcda7411efa503849ab2ee4fd93656a8fde2ac34
ggt000602-afk <ggt000602@gmail.com>
2026-08-12 14:20:30 +0900

    Add boiling surface cryogenic and experiment vocabulary part 3

---
+++ data_more3.js
@@ -0,0 +1,99 @@
+addBulkWords(`hot spot|/hɑːt spɑːt/|핫 스팟|고온점|Boiling
+wetting front|/ˈwɛtɪŋ frʌnt/|웨팅 프런트|젖음 전선|Boiling
+interfacial lift-off|/ˌɪntərˈfeɪʃəl ˈlɪft ɔːf/|인터페이셜 리프트 오프|계면 이탈|Boiling
+Kelvin-Helmholtz instability|/ˈkɛlvɪn ˈhɛlmhoʊlts ˌɪnstəˈbɪləti/|켈빈 헬름홀츠 인스터빌리티|켈빈-헬름홀츠 불안정성|Boiling
+Taylor wavelength|/ˈteɪlər ˈweɪvlɛŋθ/|테일러 웨이브렝스|테일러 파장|Boiling
+characteristic length|/ˌkærəktəˈrɪstɪk lɛŋθ/|캐릭터리스틱 렝스|특성 길이|Heat Transfer
+capillary length|/ˈkæpəleri lɛŋθ/|캐필러리 렝스|모세관 길이|Heat Transfer
+Bond number|/bɑːnd ˈnʌmbər/|본드 넘버|본드수|Heat Transfer
+Weber number|/ˈwɛbər ˈnʌmbər/|웨버 넘버|웨버수|Heat Transfer
+Reynolds number|/ˈrɛnəldz ˈnʌmbər/|레이놀즈 넘버|레이놀즈수|Heat Transfer
+Prandtl number|/ˈprændtəl ˈnʌmbər/|프란틀 넘버|프란틀수|Heat Transfer
+Jakob number|/ˈdʒɑːkəb ˈnʌmbər/|야콥 넘버|야콥수|Heat Transfer
+Nusselt number|/ˈnuːsɛlt ˈnʌmbər/|누셀트 넘버|누셀트수|Heat Transfer
+liquid nitrogen (LN2)|/ˈlɪkwɪd ˈnaɪtrədʒən/|리퀴드 나이트로젠 엘엔투|액체질소|Cryogenics
+liquid hydrogen|/ˈlɪkwɪd ˈhaɪdrədʒən/|리퀴드 하이드로젠|액체수소|Cryogenics
+liquid helium|/ˈlɪkwɪd ˈhiːliəm/|리퀴드 헬륨|액체헬륨|Cryogenics
+cryostat|/ˈkraɪəstæt/|크라이오스탯|크라이오스탯|Cryogenics
+Dewar|/ˈduːər/|듀어|듀어 용기|Cryogenics
+vacuum insulation|/ˈvækjuːm ˌɪnsəˈleɪʃən/|배큠 인슐레이션|진공단열|Cryogenics
+multilayer insulation|/ˌmʌltiˈleɪər ˌɪnsəˈleɪʃən/|멀티레이어 인슐레이션|다층단열|Cryogenics
+radiation shield|/ˌreɪdiˈeɪʃən ʃiːld/|레이디에이션 실드|복사 차폐|Cryogenics
+boil-off|/ˈbɔɪl ɔːf/|보일 오프|증발 손실|Cryogenics
+chill-down|/ˈtʃɪl daʊn/|칠다운|냉각다운, 칠다운|Cryogenics
+quenching|/ˈkwɛntʃɪŋ/|퀜칭|급랭|Cryogenics
+thermal steady state|/ˈθɜːrməl ˈstɛdi steɪt/|써멀 스테디 스테이트|열적 정상상태|Cryogenics
+cryogenic temperature|/ˌkraɪəˈdʒɛnɪk ˈtɛmprətʃər/|크라이어제닉 템퍼러처|극저온 온도|Cryogenics
+room temperature|/ruːm ˈtɛmprətʃər/|룸 템퍼러처|상온|Cryogenics
+parasitic heat load|/ˌpærəˈsɪtɪk hiːt loʊd/|패러시틱 히트 로드|기생 열부하|Cryogenics
+surface energy|/ˈsɜːrfɪs ˈɛnərdʒi/|서피스 에너지|표면에너지|Surface
+adhesion|/ədˈhiːʒən/|애드히전|접착|Surface
+cohesion|/koʊˈhiːʒən/|코히전|응집|Surface
+spreading|/ˈsprɛdɪŋ/|스프레딩|퍼짐|Surface
+contact line|/ˈkɑːntækt laɪn/|콘택트 라인|접촉선|Surface
+advancing contact angle|/ədˈvænsɪŋ ˈkɑːntækt ˈæŋɡəl/|어드밴싱 콘택트 앵글|전진 접촉각|Surface
+receding contact angle|/rɪˈsiːdɪŋ ˈkɑːntækt ˈæŋɡəl/|리시딩 콘택트 앵글|후퇴 접촉각|Surface
+contact angle hysteresis|/ˈkɑːntækt ˈæŋɡəl ˌhɪstəˈriːsɪs/|콘택트 앵글 히스테리시스|접촉각 이력|Surface
+wetted area|/ˈwɛtɪd ˈeriə/|웨티드 에어리어|젖은 면적|Surface
+dry area|/draɪ ˈeriə/|드라이 에어리어|건조 면적|Surface
+roughness ratio|/ˈrʌfnəs ˈreɪʃioʊ/|러프니스 레이쇼|거칠기 비|Surface
+cavity radius|/ˈkævəti ˈreɪdiəs/|캐비티 레이디어스|공동 반경|Surface
+cavity distribution|/ˈkævəti ˌdɪstrɪˈbjuːʃən/|캐비티 디스트리뷰션|공동 분포|Surface
+pore network|/pɔːr ˈnɛtwɜːrk/|포어 네트워크|기공 네트워크|Surface
+permeability|/ˌpɜːrmiəˈbɪləti/|퍼미어빌리티|투과도|Surface
+capillary pressure|/ˈkæpəleri ˈprɛʃər/|캐필러리 프레셔|모세관 압력|Surface
+capillary rise|/ˈkæpəleri raɪz/|캐필러리 라이즈|모세관 상승|Surface
+wicking height|/ˈwɪkɪŋ haɪt/|위킹 하이트|위킹 높이|Surface
+wicking rate|/ˈwɪkɪŋ reɪt/|위킹 레이트|위킹 속도|Surface
+oxidation|/ˌɑːksɪˈdeɪʃən/|옥시데이션|산화|Surface
+hydroxide|/haɪˈdrɑːksaɪd/|하이드록사이드|수산화물|Surface
+copper oxide|/ˈkɑːpər ˈɑːksaɪd/|카퍼 옥사이드|산화구리|Surface
+copper hydroxide|/ˈkɑːpər haɪˈdrɑːksaɪd/|카퍼 하이드록사이드|수산화구리|Surface
+electrodeposition|/ɪˌlɛktroʊˌdɛpəˈzɪʃən/|일렉트로데포지션|전착|Surface
+electrochemical treatment|/ɪˌlɛktroʊˈkɛmɪkəl ˈtriːtmənt/|일렉트로케미컬 트리트먼트|전기화학 처리|Surface
+etching|/ˈɛtʃɪŋ/|에칭|에칭|Surface
+polishing|/ˈpɑːlɪʃɪŋ/|폴리싱|연마|Surface
+acid cleaning|/ˈæsɪd ˈkliːnɪŋ/|애시드 클리닝|산세, 산 세척|Surface
+rinsing|/ˈrɪnsɪŋ/|린싱|세척, 린싱|Surface
+deionized water|/diːˈaɪənaɪzd ˈwɔːtər/|디아이오나이즈드 워터|탈이온수|Surface
+substrate|/ˈsʌbstreɪt/|섭스트레이트|기판|Surface
+coating adhesion|/ˈkoʊtɪŋ ədˈhiːʒən/|코팅 애드히전|코팅 접착력|Surface
+coating durability|/ˈkoʊtɪŋ ˌdʊrəˈbɪləti/|코팅 듀러빌리티|코팅 내구성|Surface
+feature size|/ˈfiːtʃər saɪz/|피처 사이즈|특징 크기|Surface
+fractal structure|/ˈfræktəl ˈstrʌktʃər/|프랙털 스트럭처|프랙탈 구조|Surface
+thermocouple|/ˈθɜːrmoʊˌkʌpəl/|써모커플|열전대|Experiment
+resistance thermometer|/rɪˈzɪstəns θərˈmɑːmətər/|리지스턴스 써모미터|저항 온도계|Experiment
+platinum resistance thermometer|/ˈplætɪnəm rɪˈzɪstəns θərˈmɑːmətər/|플래티넘 리지스턴스 써모미터|백금 저항 온도계|Experiment
+four-wire measurement|/fɔːr waɪər ˈmɛʒərmənt/|포 와이어 메저먼트|4선식 측정|Experiment
+two-wire measurement|/tuː waɪər ˈmɛʒərmənt/|투 와이어 메저먼트|2선식 측정|Experiment
+data acquisition system|/ˈdeɪtə ˌækwɪˈzɪʃən ˈsɪstəm/|데이터 애퀴지션 시스템|데이터 획득 시스템|Experiment
+sampling rate|/ˈsæmplɪŋ reɪt/|샘플링 레이트|샘플링 속도|Experiment
+acquisition rate|/ˌækwɪˈzɪʃən reɪt/|애퀴지션 레이트|획득 속도|Experiment
+frame rate|/freɪm reɪt/|프레임 레이트|프레임 속도|Experiment
+voltage|/ˈvoʊltɪdʒ/|볼티지|전압|Experiment
+current|/ˈkɜːrənt/|커런트|전류|Experiment
+current density|/ˈkɜːrənt ˈdɛnsəti/|커런트 덴서티|전류밀도|Experiment
+electrical power|/ɪˈlɛktrɪkəl ˈpaʊər/|일렉트리컬 파워|전력|Experiment
+power supply|/ˈpaʊər səˈplaɪ/|파워 서플라이|전원공급기|Experiment
+shunt resistor|/ʃʌnt rɪˈzɪstər/|션트 리지스터|션트 저항|Experiment
+pressure transducer|/ˈprɛʃər trænzˈduːsər/|프레셔 트랜스듀서|압력 변환기|Experiment
+flow meter|/floʊ ˈmiːtər/|플로 미터|유량계|Experiment
+test section|/tɛst ˈsɛkʃən/|테스트 섹션|시험부|Experiment
+test piece|/tɛst piːs/|테스트 피스|시험편|Experiment
+test specimen|/tɛst ˈspɛsəmən/|테스트 스페시먼|시험 시편|Experiment
+heating surface|/ˈhiːtɪŋ ˈsɜːrfɪs/|히팅 서피스|가열면|Experiment
+heater wire|/ˈhiːtər waɪər/|히터 와이어|가열 와이어|Experiment
+wire diameter|/waɪər daɪˈæmɪtər/|와이어 다이애미터|와이어 직경|Experiment
+immersion depth|/ɪˈmɜːrʒən dɛpθ/|이머전 뎁스|침지 깊이|Experiment
+liquid level|/ˈlɪkwɪd ˈlɛvəl/|리퀴드 레벨|액위|Experiment
+vessel|/ˈvɛsəl/|베슬|용기|Experiment
+vacuum chamber|/ˈvækjuːm ˈtʃeɪmbər/|배큠 챔버|진공 챔버|Experiment
+insulation jacket|/ˌɪnsəˈleɪʃən ˈdʒækɪt/|인슐레이션 재킷|단열 재킷|Experiment
+viewport|/ˈvjuːpɔːrt/|뷰포트|관찰창|Experiment
+tolerance|/ˈtɑːlərəns/|톨러런스|공차|Experiment
+instrument resolution|/ˈɪnstrəmənt ˌrɛzəˈluːʃən/|인스트루먼트 레졸루션|계측기 분해능|Experiment
+signal-to-noise ratio|/ˈsɪɡnəl tə nɔɪz ˈreɪʃioʊ/|시그널 투 노이즈 레이쇼|신호대잡음비|Experiment
+sampling interval|/ˈsæmplɪŋ ˈɪntərvəl/|샘플링 인터벌|샘플링 간격|Experiment
+time series|/taɪm ˈsɪriːz/|타임 시리즈|시계열|Experiment
+time resolution|/taɪm ˌrɛzəˈluːʃən/|타임 레졸루션|시간 분해능|Experiment
+spatial resolution|/ˈspeɪʃəl ˌrɛzəˈluːʃən/|스페이셜 레졸루션|공간 분해능|Experiment`);
